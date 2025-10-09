f3ecc7d6275aa1f9c322a9a01c9a212bc868916a
Siddhant128-bit <siddhant.sharma1974@gmail.com>
2025-10-09 17:25:06 +0545

    Class functionality is working fine

--- scrape_info.py
+++ scrape_info.py
@@ -5,98 +5,109 @@ from bs4 import BeautifulSoup
 import time
 import pandas as pd
 
-# Set up Chrome options for headless mode
-chrome_options = Options()
-# chrome_options.add_argument('--headless')  # Uncomment to run in headless mode
-chrome_options.add_argument('--no-sandbox')
-chrome_options.add_argument('--disable-dev-shm-usage')
-chrome_options.add_argument('--disable-gpu')
-chrome_options.add_argument('--remote-debugging-port=9222')
-chrome_options.add_argument('--window-size=1920x1080')
+class webscrapper:
+    def __init__(self,Driver_path=None):
+        self.Driver_path=Driver_path
 
-# Specify the path to the ChromeDriver executable
-service = Service(None)
+        # Set up Chrome options for headless mode
+        chrome_options = Options()
+        chrome_options.add_argument('--headless')  # Uncomment to run in headless mode
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--disable-dev-shm-usage')
+        chrome_options.add_argument('--disable-gpu')
+        chrome_options.add_argument('--remote-debugging-port=9222')
+        chrome_options.add_argument('--window-size=1920x1080')
 
-# Initialize the WebDriver
-driver = webdriver.Chrome(service=service, options=chrome_options)
-# Function to convert abbreviated counts (e.g., 'K', 'M') to full numbers
-def convert_abbreviated_count(count):
-    # count=count.replace('.','')
-    if 'K' in count:
-        # print((count.replace('K', '').strip()))
-        return float(count.replace('K', '').strip()) * 1000
-    elif 'M' in count:
-        return float(count.replace('M', '').strip()) * 1000000
-    return float(count.replace(',',''))
+        # Specify the path to the ChromeDriver executable
+        service = Service(self.Driver_path)
 
-# Function to extract video details (title, URL, and views)
-def get_video_details():
-    soup = BeautifulSoup(driver.page_source, 'html.parser')
-    
-    # Find all video containers
-    videos = soup.find_all('ytd-rich-grid-media')
-    # print(videos)
-    video_details = []
-    
-    for video in videos:
-        # print(video)
-        try:
-            title_tag = video.find('yt-formatted-string', {'id': 'video-title'})
-            title_tag=str(title_tag)
-            title_tag=title_tag.split('>')[1].split('<')[0]
-            url_tag = video.find('a', {'id': 'video-title-link'})
-            view_tag=video.find('span', {'class': 'inline-metadata-item style-scope ytd-video-meta-block'})
-            view_tag=str(view_tag)
-            view_tag=view_tag.split('views')[0].split('>')[-1]
-            view_tag=convert_abbreviated_count(view_tag)
-            video_details.append({
-                'title': title_tag,
-                'views': view_tag
-            })
-        except Exception as e:
-            print(e)
+        # Initialize the WebDriver
+        self.driver = webdriver.Chrome(service=service, options=chrome_options)
 
-    return video_details
 
-# Function to scroll the page
-def scroll_page(video_url="https://www.youtube.com/@VKunia/videos",channel='other_channel'):
-    driver.get(video_url)
-    if channel=='other_channel':
-        time.sleep(1.5)  # Allow the page to load
-        last_height = driver.execute_script("return document.documentElement.scrollHeight")
+    # Function to convert abbreviated counts (e.g., 'K', 'M') to full numbers
+    def convert_abbreviated_count(self,count):
+        # count=count.replace('.','')
+        if 'K' in count:
+            # print((count.replace('K', '').strip()))
+            return float(count.replace('K', '').strip()) * 1000
+        elif 'M' in count:
+            return float(count.replace('M', '').strip()) * 1000000
+        return float(count.replace(',',''))
+
+
+    # Function to extract video details (title, URL, and views)
+    def get_video_details(self):
+        soup = BeautifulSoup(self.driver.page_source, 'html.parser')
         
-        while True:
-            driver.execute_script("window.scrollTo(0, document.documentElement.scrollHeight);")
-            time.sleep(1.5)  # Allow more videos to load
-            new_height = driver.execute_script("return document.documentElement.scrollHeight")
-            if new_height == last_height:
-                break
-            
-            last_height = new_height
-    else:
-        time.sleep(1.05)
-        last_height = driver.execute_script("return document.documentElement.scrollHeight")
+        # Find all video containers
+        videos = soup.find_all('ytd-rich-grid-media')
+        # print(videos)
+        video_details = []
+        
+        for video in videos:
+            # print(video)
+            try:
+                title_tag = video.find('yt-formatted-string', {'id': 'video-title'})
+                title_tag=str(title_tag)
+                title_tag=title_tag.split('>')[1].split('<')[0]
+                url_tag = video.find('a', {'id': 'video-title-link'})
+                view_tag=video.find('span', {'class': 'inline-metadata-item style-scope ytd-video-meta-block'})
+                view_tag=str(view_tag)
+                view_tag=view_tag.split('views')[0].split('>')[-1]
+                view_tag=self.convert_abbreviated_count(view_tag)
+                video_details.append({
+                    'title': title_tag,
+                    'views': view_tag
+                })
+            except Exception as e:
+                print(e)
 
-# Function to extract the subscriber count
-def get_subscriber_count():
-    soup = BeautifulSoup(driver.page_source, 'html.parser')
-    # Find the element that contains the subscriber count
-    subscriber_data = soup.find('div', class_='yt-page-header-view-model__page-header-headline-info')
-    print(subscriber_data)
-    if subscriber_data:
-        subscriber_text = subscriber_data.get_text(strip=True)
-        # Extract only the subscriber number, excluding additional text (like 'subscribers')
-        if "subscribers" in subscriber_text:
-            subscriber_count = subscriber_text.split(' subscribers')[0]
-            return convert_abbreviated_count(subscriber_count.split('•')[1].strip())
-    return None
+        return video_details
+
+    # Function to scroll the page
+    def scroll_page(self,video_url="https://www.youtube.com/@VKunia/videos",channel='other_channel'):
+        self.driver.get(video_url)
+        if channel=='other_channel':
+            time.sleep(1.5)  # Allow the page to load
+            last_height = self.driver.execute_script("return document.documentElement.scrollHeight")
+            
+            while True:
+                self.driver.execute_script("window.scrollTo(0, document.documentElement.scrollHeight);")
+                time.sleep(1.5)  # Allow more videos to load
+                new_height = self.driver.execute_script("return document.documentElement.scrollHeight")
+                if new_height == last_height:
+                    break
+                
+                last_height = new_height
+        else:
+            time.sleep(1.05)
+            last_height = self.driver.execute_script("return document.documentElement.scrollHeight")
+
+    # Function to extract the subscriber count
+    def get_subscriber_count(self):
+        soup = BeautifulSoup(self.driver.page_source, 'html.parser')
+        # Find the element that contains the subscriber count
+        subscriber_data = soup.find('div', class_='yt-page-header-view-model__page-header-headline-info')
+        if subscriber_data:
+            subscriber_text = subscriber_data.get_text(strip=True)
+            # Extract only the subscriber number, excluding additional text (like 'subscribers')
+            if "subscribers" in subscriber_text:
+                subscriber_count = subscriber_text.split(' subscribers')[0]
+                return self.convert_abbreviated_count(subscriber_count.split('•')[1].strip())
+        return None
 
 
 if __name__=='__main__':
+    WS=webscrapper()
     our_channel="https://www.youtube.com/@VKunia/videos"
-    scroll_page(our_channel,channel='our_channel')
-    our_sub=get_subscriber_count()
+    WS.scroll_page(our_channel,channel='out_channel')
+    our_sub=WS.get_subscriber_count()
     print(f'Sub Count: {our_sub}')
+    
+    # scroll_page(our_channel,channel='our_channel')
+    # our_sub=get_subscriber_count()
+    # print(f'Sub Count: {our_sub}')
     # other_channels=[
     #     "https://www.youtube.com/@NatalieGoldReacts/videos",
     #     "https://www.youtube.com/@PopcornInBed/videos",
